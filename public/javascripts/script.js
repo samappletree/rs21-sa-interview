56a280885c543dbaa0207be1d36479990242cd8e
Sam Applebaum <samappletree@gmail.com>
2017-03-31 19:49:08 -0700

    Added more info on page including map key

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,12 +1,11 @@
 //script.js
 var map;
-map = L.map('mapid').setView([35.0853, -106.6056], 13);
+map = L.map('mapid').setView([35.0844444, -106.6505556], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2FtYXBsZXRyZWUiLCJhIjoiY2owdHhnMG52MDU3dTJxcWE5OWQ2dzJqMSJ9.2eIWFW2BvJ-M2uq2-KwhIw', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
 }).addTo(map);
-
 
 function onAddCensusClick() {
 	$.getJSON('data/BernallioCensusBlocks_Joined.json', function(censusData){
@@ -71,13 +70,17 @@ function onAddTwitterClick() {
 			iconSize: [60,50]
 		});
 
-		L.geoJson(twitterJSON, {
+		var tweets = L.geoJson(twitterJSON, {
 			pointToLayer: function(feature,latlng){
 	      var marker = L.marker(latlng,{icon: twitterIcon});
 	      marker.bindPopup("@" + feature.properties.Username + ' says <br/> "' + feature.properties.Tweet +'"');
 	      return marker;
 	    }
-		}).addTo(map);
+		});
+
+		var clusters = L.markerClusterGroup();
+		clusters.addLayer(tweets);
+		map.addLayer(clusters);
 
 	});
 }
@@ -101,10 +104,13 @@ function onAddFBClick() {
 		}, fbJSONg5 = {
 			"type": "FeatureCollection",
 			"features": []
+		}, fbJSONg6 = {
+			"type": "FeatureCollection",
+			"features": []
 		};
 
 		var lines = fbRawData.data;
-		console.log((lines.length - 2));
+		
 		for (var i = (lines.length - 2); i >= 0; i--) {
 			var currentline = lines[i].filter(function(v){return v!==''});;
 			var obj = {
@@ -133,73 +139,97 @@ function onAddFBClick() {
 					obj.properties.Checkins = Number(currentline[2]);
 					break;
 			}
-			if (obj.properties.Checkins >= 150) {
-				if(obj.properties.Category.includes("Health/medical/pharmacy")) fbJSONg1.features.push(obj);
-				else if (obj.properties.Category.includes("Local business")) fbJSONg2.features.push(obj);
-				else if (obj.properties.Category.includes("Real estate")) fbJSONg3.features.push(obj);
-				else if (obj.properties.Category.includes("Landmark")) fbJSONg4.features.push(obj);
-				else if (obj.properties.Category.includes("Restaurant/cafe")) fbJSONg5.features.push(obj);
-			}
+			
+			if(obj.properties.Category.includes("Health/medical/pharmacy")) fbJSONg1.features.push(obj);
+			else if (obj.properties.Category.includes("Real estate")) fbJSONg3.features.push(obj);
+			else if (obj.properties.Category.includes("Landmark")) fbJSONg4.features.push(obj);
+			else if (obj.properties.Category.includes("Restaurant/cafe")) fbJSONg5.features.push(obj);
+			else if (obj.properties.Category.includes("Education") ||obj.properties.Category.includes("School") || obj.properties.Category.includes("University")) fbJSONg6.features.push(obj);
+			else fbJSONg2.features.push(obj);
 			
 		}
+		var clusters = L.markerClusterGroup();
+
 		var healthIcon = L.icon({
 			iconUrl: '/images/health.png',
 			iconSize: [60,50]
 		});
-		L.geoJson(fbJSONg1,  {
+		var health = L.geoJson(fbJSONg1,  {
 			pointToLayer: function(feature,latlng){
 		      var marker = L.marker(latlng,{icon: healthIcon});
 		      marker.bindPopup(feature.properties["Name of Place"] + "<br/>Total Facebook Checkins: " + feature.properties.Checkins);
 		      return marker;
 		  }
-	    }).addTo(map);
-	    
+	    });
+	    clusters.addLayer(health);
+
 	    var realestateIcon = L.icon({
 			iconUrl: '/images/realestate.png',
 			iconSize: [60,50]
 		});
-	    L.geoJson(fbJSONg3,  {
+	    var realestate = L.geoJson(fbJSONg3,  {
 			pointToLayer: function(feature,latlng){
 		      var marker = L.marker(latlng,{icon: realestateIcon});
 		      marker.bindPopup(feature.properties["Name of Place"] + "<br/>Total Facebook Checkins: " + feature.properties.Checkins);
 		      return marker;
 		  }
-	    }).addTo(map);
+	    });
+	    clusters.addLayer(realestate);
 	    
 	    var landmarkIcon = L.icon({
 			iconUrl: '/images/landmark.png',
 			iconSize: [60,50]
 		});
-	    L.geoJson(fbJSONg4,  {
+	    var landmark = L.geoJson(fbJSONg4,  {
 			pointToLayer: function(feature,latlng){
 		      var marker = L.marker(latlng,{icon: landmarkIcon});
 		      marker.bindPopup(feature.properties["Name of Place"] + "<br/>Total Facebook Checkins: " + feature.properties.Checkins);
 		      return marker;
 		  }
-	    }).addTo(map);
+	    });
+	    clusters.addLayer(landmark);
+
 	    var restaurantIcon = L.icon({
 			iconUrl: '/images/restaurant.png',
 			iconSize: [60,50]
 		});
-	    L.geoJson(fbJSONg5,  {
+	   var restaurant =  L.geoJson(fbJSONg5,  {
 			pointToLayer: function(feature,latlng){
 		      var marker = L.marker(latlng,{icon: restaurantIcon});
 		      marker.bindPopup(feature.properties["Name of Place"] + "<br/>Total Facebook Checkins: " + feature.properties.Checkins);
 		      return marker;
 		  }
-	    }).addTo(map);
+	    });
+	   	clusters.addLayer(restaurant);
 
-		L.geoJson(fbJSONg2,  {
+	    var schoolIcon = L.icon({
+			iconUrl: '/images/school.png',
+			iconSize: [60,50]
+		});
+	    var school = L.geoJson(fbJSONg6,  {
+			pointToLayer: function(feature,latlng){
+		      var marker = L.marker(latlng,{icon: schoolIcon});
+		      marker.bindPopup(feature.properties["Name of Place"] + "<br/>Total Facebook Checkins: " + feature.properties.Checkins);
+		      return marker;
+		  }
+	    });
+	    clusters.addLayer(school);
+
+		var local = L.geoJson(fbJSONg2,  {
 			onEachFeature: function(feature, layer){
 				layer.bindPopup(feature.properties["Name of Place"] + "<br/>Total Facebook Checkins: " + feature.properties.Checkins);
 	   		}
-	    }).addTo(map);
+	    });
+	    clusters.addLayer(local);
+
+		map.addLayer(clusters);
+
 	});
 }
 
 function clearMap(){
 	map.remove();
-	map = L.map('mapid').setView([35.0853, -106.6056], 13);
+	map = L.map('mapid').setView([35.0844444, -106.6505556], 13);
 	L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2FtYXBsZXRyZWUiLCJhIjoiY2owdHhnMG52MDU3dTJxcWE5OWQ2dzJqMSJ9.2eIWFW2BvJ-M2uq2-KwhIw', {
 	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
 	    maxZoom: 18,
